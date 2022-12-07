717524c1faa8fa7bddb08dccaf805b91f83b20ac
gmartineza <68785609+gmartineza@users.noreply.github.com>
2022-12-07 03:35:31 -0300

    Delete dotconfig/nvim/lua directory

--- dotconfig/nvim/lua/plugins.lua
+++
@@ -1,7 +0,0 @@
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'gruvbox-community/gruvbox'
-    use 'jiangmiao/auto-pairs'
-    use 'mhinz/vim-startify'
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-end)
