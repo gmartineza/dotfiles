d50620e6b011886c03ee026fa839132b426b80d4
gmartineza <gabymartinezalarcon@gmail.com>
2023-10-02 21:25:13 -0300

    update awesome config

--- dotconfig/awesome/rc.lua
+++ dotconfig/awesome/rc.lua
@@ -53,8 +53,8 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.useless_gap = 6
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
-editor = "nvim" or os.getenv("EDITOR")
+terminal = "xterm"
+editor = "vim" or os.getenv("EDITOR")
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -62,7 +62,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
