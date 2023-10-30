62e621bbdce25f3effca71d86773507b9654d1be
gmartineza <68785609+gmartineza@users.noreply.github.com>
2023-10-30 01:41:59 -0300

    Add files via upload

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -1,3 +1,54 @@
-require("gmartineza.set")
-require("gmartineza.remap")
-require("gmartineza.packer")
+vim.g.mapleader=' '
+vim.keymap.set('n','<leader>e',vim.cmd.Ex)
+vim.cmd('set path+=**')
+vim.keymap.set('n','<leader>n',vim.cmd.cnext)
+vim.keymap.set('n','<leader>N',vim.cmd.cNext)
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+	},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+})
+
+require'nvim-treesitter.configs'.setup {
+	auto_install = true,
+	highlight = { enable = true},
+	indent = { enable = true}
+}
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
