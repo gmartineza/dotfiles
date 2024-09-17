84b5ed562b11506e7471774a5d0cf55940f6985a
gmartineza <gabymartinezalarcon@gmail.com>
2024-09-17 10:20:30 -0300

    biggie update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,12 +8,14 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.scrolloff = 4
 vim.opt.breakindent = true
+vim.opt.autochdir = true
 -- vim.opt.conceallevel = 2
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Key mappings
+vim.keymap.set("n", "<Space>", "<nop>")
 vim.keymap.set("n", "<C-s>", "<cmd>write<CR>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
@@ -28,6 +30,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "<leader>f", ":find ")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -45,14 +48,14 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         "kylechui/nvim-surround",
-        version = "*", -- For stability
-        opts = {},
-        -- event = "VeryLazy",
+        version = "*",
+        event = "VeryLazy",
+        opts = {}
     },
+
     {
         "numToStr/Comment.nvim",
-        opts = {},
-        lazy = false,
+        opts = {}
     },
 
     {
@@ -65,40 +68,34 @@ require("lazy").setup({
         "calind/selenized.nvim",
         config = function()
             vim.cmd.colorscheme "selenized"
-        end,
-    },
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function () 
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                highlight = { enable = true },
-                indent = { enable = true },  
-            })
         end
     },
 
     {
-        "tadmccorkle/markdown.nvim",
-        ft = "markdown", -- or 'event = "VeryLazy"'
-        config = function()
-                require("markdown").setup({
-                on_attach = function(bufnr)
-                    local map = vim.keymap.set
-                    local opts = { buffer = bufnr }
-                    map({ 'n', 'i' }, '<M-o>', '<Cmd>MDListItemBelow<CR>', opts)
-                    map({ 'n', 'i' }, '<M-O>', '<Cmd>MDListItemAbove<CR>', opts)
-                    map({ 'n', 'i' }, '<M-l><M-r>', '<Cmd>MDResetListNumbering<CR>', opts)
-                    map('n', '<M-l><M-t>', '<Cmd>MDTaskToggle<CR>', opts)
-                    map('x', '<M-l><M-t>', ':MDTaskToggle<CR>', opts)
-                end,
-            })
-    end
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        ---@type Flash.Config
+        opts = {
+            jump = {
+                autojump = true,
+            },
+            modes = {
+                char = {
+                    enabled = false,
+                },
+            },
+        },
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
     },
-    { import = 'obsidian' }
+
+    -- { import = 'obsidian' }
 })
 
 if not vim.g.vscode then
@@ -106,6 +103,4 @@ if not vim.g.vscode then
     vim.opt.shiftwidth = 4
     vim.opt.expandtab = true
     vim.opt.number = true
-
-    -- vim.keymap.set("n", "<leader>g", ":vim /")
 end
--- .config/nvim/lua/obsidian/cmp.lua
+++ .config/nvim/lua/obsidian/cmp.lua
@@ -1,10 +1,8 @@
 return {
     "hrsh7th/nvim-cmp",
+    ft = "markdown",
     config = function()
         local cmp = require('cmp')
-        -- cmp.setup({
-        --     sources = {},
-        -- })
         cmp.setup({
             sources = cmp.config.sources({
                 { name = 'buffer' },
