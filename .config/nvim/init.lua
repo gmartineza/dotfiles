0b0f3e026be2713741eebeb0a19b637ffa9b7e06
gmartineza <gabymartinezalarcon@gmail.com>
2025-04-19 14:25:47 -0300

    added obsidian.nvim with cmp working

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,65 +2,66 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- VSCode-Neovim stuff --
+-- conditional sets: --
+-- if vim.g.vscode then
+--     -- VSCode extension
+-- else
+--     -- ordinary Neovim
+-- end
+-- conditional plugins --
+-- cond = vim.g.vscode
+
 -- [[ Setting options ]]
-vim.opt.number = true
-vim.opt.relativenumber = true
--- vim.opt.breakindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+if not vim.g.vscode then
+  vim.opt.number = true
+  -- vim.opt.breakindent = true
+  vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.cursorline = true
+  vim.opt.scrolloff = 5
+  vim.opt.wrap = false
+  vim.keymap.set("n", "<leader>e", "<cmd>Explore<CR>")
+  vim.keymap.set("n", "<leader>,", "<cmd>e $MYVIMRC<CR>")
+end
+
 vim.opt.undofile = true
+vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.cursorline = true
-vim.opt.scrolloff = 5
-vim.opt.hlsearch = false
 
 -- [[ Basic Keymaps ]]
 -- Heressy
 vim.keymap.set("n", "<C-s>", "<cmd>write<CR>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", "<cmd>nohl<CR>")
 
 -- prime's stuff
 vim.keymap.set({"n", "v"},  "<leader>y", [["+y]])
 vim.keymap.set("n",         "<leader>Y", [["+Y]])
+vim.keymap.set("n",         "<leader>p", [["+p]])
+vim.keymap.set("n",         "<leader>P", [["+P]])
 vim.keymap.set({"n", "v"},  "<leader>d", [["_d]])
-vim.keymap.set("x",         "<leader>p", [["_dP]])
 
-vim.keymap.set("n", "<leader>e", "<cmd>Explore<CR>")
-vim.keymap.set("n", "<leader>,", "<cmd>e $MYVIMRC<CR>")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 require("lazy").setup({
   { "tpope/vim-surround" },
-
-  {
-    "easymotion/vim-easymotion",
-    keys = {
-      { "s", "<Plug>(easymotion-s2)", mode = "n", noremap = true, silent = true },
-    },
-    lazy = true,
-    event = "VeryLazy",
-  },
-
-  {
-    "arcticicestudio/nord-vim",
-    cond = (function() return not vim.g.vscode end),
-    priority = 1000,
-    config = function ()
-      vim.cmd.colorscheme('nord')
-    end
-  },
+  { "tpope/vim-repeat" },
+  { import = "terminal-plugins", 
+    cond = not vim.g.vscode
+  }
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
