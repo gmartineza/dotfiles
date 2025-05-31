e6f93ef8b250ae1a8e896c86392dd01845f3cb62
gmartineza <gabymartinezalarcon@gmail.com>
2025-05-31 19:00:01 -0300

    fix vault fzf mapping

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -6,7 +6,7 @@ return {
       vim.keymap.set("n", "<c-p>", "<cmd>FZF<CR>")
       vim.keymap.set("n", "<leader>s,", "<cmd>FZF ~/dotfiles/.config/nvim/<CR>")
       vim.keymap.set("n", "<leader>s.", "<cmd>FZF ~/dotfiles/.config/<CR>")
-      vim.keymap.set("n", "<leader>sn", "<cmd>FZF ~/dotfiles/.config/<CR>")
+      vim.keymap.set("n", "<leader>sn", "<cmd>FZF ~/Syncthing/notes<CR>")
       vim.keymap.set("n", "<leader>sh", "<cmd>FZF %:h<CR>")
     end
   }
