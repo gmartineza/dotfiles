5eb767edc5fd53b4a8e2edfc911942d2daf985ed
gmartineza <gabymartinezalarcon@gmail.com>
2025-05-31 18:54:08 -0300

    add fzf remaps

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,6 +4,10 @@ return {
     dependencies = { "junegunn/fzf" },
     config = function()
       vim.keymap.set("n", "<c-p>", "<cmd>FZF<CR>")
+      vim.keymap.set("n", "<leader>s,", "<cmd>FZF ~/dotfiles/.config/nvim/<CR>")
+      vim.keymap.set("n", "<leader>s.", "<cmd>FZF ~/dotfiles/.config/<CR>")
+      vim.keymap.set("n", "<leader>sn", "<cmd>FZF ~/dotfiles/.config/<CR>")
+      vim.keymap.set("n", "<leader>sh", "<cmd>FZF %:h<CR>")
     end
   }
 }
