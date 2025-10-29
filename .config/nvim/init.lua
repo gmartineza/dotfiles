3ed46c9acce68b78e84160beef9b33496036ca2f
gmartineza <gabymartinezalarcon@gmail.com>
2025-10-29 11:08:25 -0300

    vscodenvim and x leap enjoyer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,51 +2,67 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- VSCode-Neovim conditional options
--- if vim.g.vscode then
---     -- VSCode extension
--- else
---     -- ordinary Neovim
--- end
+--[[ VSCode keybindings.json
 
--- VSCode-Neovim conditional plugins w/Lazy.nvim
--- cond = vim.g.vscode
+[
+    {
+        "key": "ctrl+tab",
+        "command": "workbench.action.nextEditor",
+        "when": "!activeEditorGroupEmpty"
+    },
+    {
+        "key": "ctrl+shift+tab",
+        "command": "workbench.action.previousEditor",
+        "when": "!activeEditorGroupEmpty"
+    },
+    {
+        "key": "ctrl+tab",
+        "command": "-workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup",
+        "when": "!activeEditorGroupEmpty"
+    },
+    {
+        "key": "ctrl+shift+tab",
+        "command": "-workbench.action.quickOpenLeastRecentlyUsedEditorInGroup",
+        "when": "!activeEditorGroupEmpty"
+    },
+    {
+        "key": "alt+t",
+        "command": "workbench.action.terminal.toggleTerminal"
+    },
+    {
+        "key": "alt+b",
+        "command": "workbench.action.toggleSidebarVisibility"
+    },
+]
 
+Also add to "terminal.integrated.commandsToSkipShell": "workbench.action.toggleSidebarVisibility" and "workbench.action.terminal.toggleTerminal"
+
+--]]
 -- [[ Setting options ]]
-if not vim.g.vscode then
-  vim.opt.number = true
-  -- vim.opt.breakindent = true
-  vim.opt.cursorline = true
-  vim.opt.scrolloff = 5
-  vim.opt.wrap = false
-  -- vim.opt.clipboard = { "unnamed", "unnamedplus" }
-  vim.opt.path:append('**')
-  vim.opt.undofile = true
-
-  vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
-  vim.keymap.set("n", "<leader>,", "<cmd>e $MYVIMRC<CR>")
-  -- vim.keymap.set("n", "<leader>sf", ":find ")
-  -- vim.keymap.set("n", "<leader>s.", ":cd %:h<CR>:find ")
-  -- vim.keymap.set("n", "<leader>sb", ":buffer ")
-  vim.keymap.set("n", "<leader><tab>", "<cmd>bn<CR>")
-  vim.keymap.set("n", "<leader><S-tab>", "<cmd>bp<CR>")
-  vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-end
-
+vim.opt.number = true
+vim.opt.scrolloff = 5
+vim.opt.wrap = false
+vim.opt.tabstop=4
+vim.opt.shiftwidth=4
+-- vim.opt.clipboard = { "unnamed", "unnamedplus" }
+vim.opt.path:append('**')
+vim.opt.undofile = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.swapfile = false
 
 -- [[ Keymaps ]]
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
+vim.keymap.set("n", "<leader>,", "<cmd>e $MYVIMRC<CR>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc><cmd>w<CR>")
 vim.keymap.set("n", "<C-c>", "<cmd>nohl<CR>")
 vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
 -- vim.keymap.set({"n", "v"},  "<leader>d", [["_d]])
--- -- vim.keymap.set("v",         "<leader>p", [["_dP]])
--- -- prime's stuffMore actions
+-- vim.keymap.set("v",         "<leader>p", [["_dP]])
+-- -- prime's stuff
 vim.keymap.set({"n", "v"},  "<leader>y", [["+y]])
 vim.keymap.set("n",         "<leader>Y", [["+Y]])
 vim.keymap.set("n",         "<leader>p", [["+p]])
@@ -72,10 +88,24 @@ require("lazy").setup({
     dependencies = "tpope/vim-repeat",
   },
   {
-    import = "plugins",
+    "windwp/nvim-autopairs",
     enabled = true,
-    cond = not vim.g.vscode
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
   },
+  {
+    "ggandor/leap.nvim",
+    dependencies = {
+      "tpope/vim-repeat"
+    },
+    config = function()
+      vim.keymap.set({'n', 'x', 'o'}, 'x', '<Plug>(leap-forward)')
+      vim.keymap.set({'n', 'x', 'o'}, 'X', '<Plug>(leap-backward)')
+      vim.keymap.set({'n', 'x', 'o'}, 'gx', '<Plug>(leap-from-window)')
+    end
+  }
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
