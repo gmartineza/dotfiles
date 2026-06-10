1034bb6605736548e94d92d9cd3fd66ad2d25ea5
gmartineza <68785609+gmartineza@users.noreply.github.com>
2026-06-10 12:06:48 -0300

    fix(oil): restore :Ex & show hidden files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,13 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    default_file_explorer = false,
+    view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = false,
+    }
+  },
   lazy = false,
   keys = {
     { '<leader>e', '<cmd>Oil<CR>', desc = 'Open Oil' },
